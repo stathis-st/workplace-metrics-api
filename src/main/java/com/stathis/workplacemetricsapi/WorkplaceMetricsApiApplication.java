1f817cd47c2c8dd12e602be553d53879dfd958c1
stathis-st <stathis.stamatis@gmail.com>
2020-11-28 13:51:17 +0200

    Initial commit

---
+++ src/main/java/com/stathis/workplacemetricsapi/WorkplaceMetricsApiApplication.java
@@ -0,0 +1,13 @@
+package com.stathis.workplacemetricsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkplaceMetricsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkplaceMetricsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stathis/workplacemetricsapi/WorkplaceMetricsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stathis.workplacemetricsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkplaceMetricsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
