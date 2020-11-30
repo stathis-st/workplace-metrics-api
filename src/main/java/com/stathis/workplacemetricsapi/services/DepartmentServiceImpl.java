6f9a27de563ae357dfdac108c12fbcf2e1f715b8
stathis-st <stathis.stamatis@gmail.com>
2020-11-30 17:28:55 +0200

    Update implementation class of Department service.

--- src/main/java/com/stathis/workplacemetricsapi/services/DepartmentServiceImpl.java
+++ src/main/java/com/stathis/workplacemetricsapi/services/DepartmentServiceImpl.java
@@ -1,24 +1,25 @@
 package com.stathis.workplacemetricsapi.services;
 
 import com.stathis.workplacemetricsapi.domain.Department;
+import com.stathis.workplacemetricsapi.exception.ResourceNotDeletedException;
 import com.stathis.workplacemetricsapi.exception.ResourceNotFoundException;
 import com.stathis.workplacemetricsapi.exception.ResourceNotUpdatedException;
 import com.stathis.workplacemetricsapi.repositories.DepartmentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.stathis.workplacemetricsapi.exception.ResourceNotDeletedException.RESOURCE_COULD_NOT_BE_DELETED;
 import static com.stathis.workplacemetricsapi.exception.ResourceNotFoundException.RESOURCE_NOT_FOUND_FOR_ID;
-import static com.stathis.workplacemetricsapi.exception.ResourceNotUpdatedException.COULD_NOT_BE_UPDATED;
+import static com.stathis.workplacemetricsapi.exception.ResourceNotUpdatedException.RESOURCE_COULD_NOT_BE_UPDATED;
 
 @Service
+@AllArgsConstructor
 public class DepartmentServiceImpl implements DepartmentService {
 
     private final DepartmentRepository departmentRepository;
-
-    public DepartmentServiceImpl(DepartmentRepository departmentRepository) {
-        this.departmentRepository = departmentRepository;
-    }
 
     @Override
     public List<Department> getAllDepartments() {
@@ -42,7 +43,7 @@ public class DepartmentServiceImpl implements DepartmentService {
         try {
             savedDepartment = getDepartmentById(id);
         } catch (ResourceNotFoundException ex) {
-            throw new ResourceNotUpdatedException(COULD_NOT_BE_UPDATED + ex.getMessage());
+            throw new ResourceNotUpdatedException(RESOURCE_COULD_NOT_BE_UPDATED + ex.getMessage());
         }
         savedDepartment.setName(department.getName());
         return saveDepartment(savedDepartment);
@@ -50,6 +51,10 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     @Override
     public void deleteDepartmentById(Long id) {
-        departmentRepository.deleteById(id);
+        try {
+            departmentRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException ex) {
+            throw new ResourceNotDeletedException(RESOURCE_COULD_NOT_BE_DELETED + id);
+        }
     }
 }
