cce2bba869c573aa9030c7515603c93b99f2c08e
stathis-st <stathis.stamatis@gmail.com>
2020-12-03 11:13:07 +0200

    Add Metric Service.

---
+++ src/main/java/com/stathis/workplacemetricsapi/services/MetricService.java
@@ -0,0 +1,19 @@
+package com.stathis.workplacemetricsapi.services;
+
+import com.stathis.workplacemetricsapi.domain.Metric;
+import com.stathis.workplacemetricsapi.model.ResponseEntityWrapper;
+
+import java.awt.print.Pageable;
+
+public interface MetricService {
+
+    ResponseEntityWrapper<Metric> getAllMetrics(Pageable pageable);
+
+    Metric getMetricById(Long id);
+
+    Metric saveMetric(Metric metric);
+
+    Metric updateMetric(Long id, Metric metric);
+
+    void deleteMetricById(Long id);
+}
