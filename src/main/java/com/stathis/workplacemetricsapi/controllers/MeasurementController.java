feaa729375115ac4300bd7c99a01e98f81295c39
stathis-st <stathis.stamatis@gmail.com>
2020-12-03 14:43:16 +0200

    Add Measurement controller.

---
+++ src/main/java/com/stathis/workplacemetricsapi/controllers/MeasurementController.java
@@ -0,0 +1,43 @@
+package com.stathis.workplacemetricsapi.controllers;
+
+import com.stathis.workplacemetricsapi.domain.Measurement;
+import com.stathis.workplacemetricsapi.model.ResponseEntityWrapper;
+import com.stathis.workplacemetricsapi.services.MeasurementService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(MeasurementController.BASE_URL)
+@AllArgsConstructor
+public class MeasurementController {
+
+    public static final String BASE_URL = "/api/measurements";
+
+    private final MeasurementService measurementService;
+
+    @GetMapping
+    public ResponseEntityWrapper<Measurement> getAllMeasurements(@RequestParam(defaultValue = "0") Integer page,
+                                                                 @RequestParam(defaultValue = "10") Integer size) {
+        return measurementService.getAllMeasurements(PageRequest.of(page, size));
+    }
+
+    @GetMapping("/{id}")
+    public Measurement getMeasurementById(@PathVariable("id") Long id) {
+        return measurementService.getMeasurementById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Measurement saveMeasurement(@RequestBody Measurement measurement) {
+        return measurementService.saveMeasurement(measurement);
+    }
+}
