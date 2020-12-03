30b5aafe2f546dda5703e40797cf0f904c6f8d36
stathis-st <stathis.stamatis@gmail.com>
2020-12-03 21:54:29 +0200

    Add query method in Measurement repository to find all Measurements by Metric, Department and Measurement timestamp.

--- src/main/java/com/stathis/workplacemetricsapi/repositories/MeasurementRepository.java
+++ src/main/java/com/stathis/workplacemetricsapi/repositories/MeasurementRepository.java
@@ -1,7 +1,19 @@
 package com.stathis.workplacemetricsapi.repositories;
 
+import com.stathis.workplacemetricsapi.domain.Department;
 import com.stathis.workplacemetricsapi.domain.Measurement;
+import com.stathis.workplacemetricsapi.domain.Metric;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.ZonedDateTime;
+
 public interface MeasurementRepository extends JpaRepository<Measurement, Long> {
+
+    Page<Measurement> findMeasurementsByMetricAndDepartmentAndMeasurementTimestampBetween(Pageable pageable,
+                                                                                          Metric metric,
+                                                                                          Department department,
+                                                                                          ZonedDateTime fromDateTime,
+                                                                                          ZonedDateTime toDateTime);
 }
