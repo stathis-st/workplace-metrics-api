0c0200e2e15113ce838f3768f9053d3ab9344ae8
stathis-st <stathis.stamatis@gmail.com>
2020-12-03 14:14:16 +0200

    Add Measurement service.

---
+++ src/main/java/com/stathis/workplacemetricsapi/services/MeasurementService.java
@@ -0,0 +1,14 @@
+package com.stathis.workplacemetricsapi.services;
+
+import com.stathis.workplacemetricsapi.domain.Measurement;
+import com.stathis.workplacemetricsapi.model.ResponseEntityWrapper;
+import org.springframework.data.domain.Pageable;
+
+public interface MeasurementService {
+
+    ResponseEntityWrapper<Measurement> getAllMeasurements(Pageable pageable);
+
+    Measurement getMeasurementById(Long id);
+
+    Measurement saveMeasurement(Measurement measurement);
+}
