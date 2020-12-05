40e7443b5bbb7080eede89609b03a1171021c3c3
stathis-st <stathis.stamatis@gmail.com>
2020-12-05 13:54:02 +0200

    Update AggregatedResult and MeasurementDTO classes with @Builder annotation.

--- src/main/java/com/stathis/workplacemetricsapi/model/AggregatedResult.java
+++ src/main/java/com/stathis/workplacemetricsapi/model/AggregatedResult.java
@@ -1,12 +1,14 @@
 package com.stathis.workplacemetricsapi.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AggregatedResult {
 
     private Double averageValue;
