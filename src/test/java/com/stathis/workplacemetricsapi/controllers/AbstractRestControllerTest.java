492dea5ecede34ada9b3489501e7ca3e20619714
stathis-st <stathis.stamatis@gmail.com>
2020-12-05 18:48:56 +0200

    Update ObjectMapper in AbstractRestControllerTest in order to serialize timestamps.

--- src/test/java/com/stathis/workplacemetricsapi/controllers/AbstractRestControllerTest.java
+++ src/test/java/com/stathis/workplacemetricsapi/controllers/AbstractRestControllerTest.java
@@ -1,12 +1,23 @@
 package com.stathis.workplacemetricsapi.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public abstract class AbstractRestControllerTest {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        objectMapper.registerModule(new Jdk8Module());
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
+
     public static String asJsonString(final Object obj) {
         try {
-            return new ObjectMapper().writeValueAsString(obj);
+            return objectMapper.writeValueAsString(obj);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
